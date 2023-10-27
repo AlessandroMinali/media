1f9486e082e289a792342a11ff8ff9b17dd1de74
AlessandroMinali <4143332+AlessandroMinali@users.noreply.github.com>
2023-10-26 21:54:30 -0400

    update

--- music/spotify.rb
+++ music/spotify.rb
@@ -11,11 +11,18 @@ end
 Signal.trap('INT') { save }
 
 puts 'Spotify Song List'
+count = 100
+n = 5
 loop do
   print '>> '
   save if gets.chomp == 'exit'
-  song = DATA.shift
-  puts song
-  `open "https://yewtu.be/search?q=#{song}"`
-  # `open "https://vid.puffyan.us/search?q=#{song}"`
+  n.times do
+    song = DATA.shift
+    puts song
+    `open "https://yewtu.be/search?q=#{song}"`
+    # `open "https://vid.puffyan.us/search?q=#{song}"`
+  end
+  count -= n
+  puts count
+  save if count <= 0
 end
