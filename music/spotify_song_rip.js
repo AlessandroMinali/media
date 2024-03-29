a80f8f0db0daaeaf267cf09cd16657c5b9db0b41
AlessandroMinali <4143332+AlessandroMinali@users.noreply.github.com>
2024-03-28 20:51:17 -0400

    bcamp and spotify

--- music/spotify_song_rip.js
+++
@@ -1,5 +0,0 @@
-Array.from(
-  document.querySelectorAll(
-    'div[data-testid="playlist-tracklist"] div[role="row"][aria-selected="false"]'
-  )
-).map(x => x.innerText.split("\n\n")[1].replaceAll("\n", " ")).join("\n");
